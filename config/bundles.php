938c7b657589b208bff53c059d8c973d39fe8f09
Zygmunt Łata <zygmuntlata39@gmail.com>
2026-02-11 00:21:48 +0100

    chore: setup DDD structure and install required packages

--- config/bundles.php
+++ config/bundles.php
@@ -1,57 +1,17 @@
 <?php
 
-declare(strict_types=1);
-
-use ApiPlatform\Symfony\Bundle\ApiPlatformBundle;
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
-use Nelmio\CorsBundle\NelmioCorsBundle;
-use Symfony\Bundle\DebugBundle\DebugBundle;
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Bundle\MakerBundle\MakerBundle;
-use Symfony\Bundle\MercureBundle\MercureBundle;
-use Symfony\Bundle\MonologBundle\MonologBundle;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
-use Symfony\Bundle\TwigBundle\TwigBundle;
-use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
-
 return [
-    FrameworkBundle::class => [
-        'all' => true,
-    ],
-    SecurityBundle::class => [
-        'all' => true,
-    ],
-    MercureBundle::class => [
-        'all' => true,
-    ],
-    TwigBundle::class => [
-        'all' => true,
-    ],
-    DoctrineBundle::class => [
-        'all' => true,
-    ],
-    ApiPlatformBundle::class => [
-        'all' => true,
-    ],
-    NelmioCorsBundle::class => [
-        'all' => true,
-    ],
-    WebProfilerBundle::class => [
-        'dev' => true,
-        'test' => true,
-    ],
-    MakerBundle::class => [
-        'dev' => true,
-    ],
-    DoctrineMigrationsBundle::class => [
-        'all' => true,
-    ],
-    MonologBundle::class => [
-        'all' => true,
-    ],
-    DebugBundle::class => [
-        'dev' => true,
-        'test' => true,
-    ],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
