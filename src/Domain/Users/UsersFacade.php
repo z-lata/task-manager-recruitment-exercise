ccf055e21c27b9b0e1a90822205a2a36577a1b68
Zygmunt Łata <zygmuntlata39@gmail.com>
2026-02-12 12:54:49 +0100

    feat(users): introduce `UsersFacade` to centralize user operations

---
+++ src/Domain/Users/UsersFacade.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Users;
+
+use App\Application\Users\DTO\Model\UserDTO;
+use App\Domain\Users\Contract\Service\CreateUserServiceInterface;
+use App\Infrastructure\Users\Persistence\Store\UsersStore;
+
+final readonly class UsersFacade
+{
+    public function __construct(
+        private CreateUserServiceInterface $createUserService,
+        private UsersStore $usersStore,
+    ) {
+    }
+
+    public function createUser(UserDTO $userDTO): void
+    {
+        $this->createUserService->createUser($userDTO);
+    }
+
+    public function fetchCurrentUserWithDetails(): UserDTO
+    {
+        return $this->usersStore->fetchCurrentUserWithDetails();
+    }
+
+    /**
+     * @return UserDTO[]
+     */
+    public function fetchUsersWithDetails(): array
+    {
+        return $this->usersStore->fetchUsersWithDetails();
+    }
+}
